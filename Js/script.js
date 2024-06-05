dc40767f1d24ca02cc6bc65b4eaab1af61bf0c8c
saintcarti <xa.liberona@duocuc.cl>
2024-06-05 02:52:30 -0400

    Agregue formulario y mensaje de advertencia

--- Js/script.js
+++ Js/script.js
@@ -10,14 +10,15 @@ function mostrarAlumnos() {
       let body = "";
       for (var i = 0; i < data.length; i++) {
           body += `<tr>
-              <td class="table-success" style="background-color: white; border-top-left-radius: 15px; border-bottom-left-radius:15px; border: solid 2px black; color: black; font-size: 11px;">${data[i].Nombre_completo}</td>
-              <td class="table-success" style="background-color: white; border: solid 2px black; color: black; font-size: 11px;">${data[i].Rut}</td>
-              <td class="table-success" style="background-color: white; border: solid 2px black; color: black; font-size: 11px;">${data[i].Carrera}</td>
-              <td class="table-success" style="background-color: white; border: solid 2px black; color: black; font-size: 11px;">${data[i].Hora_entrada}</td>
-              <td class="table-success" style="background-color: white; border: solid 2px black; color: black; font-size: 11px;">${data[i].Hora_salida}</td>
-              <td class="table-success" style="background-color: white; border: solid 2px black; color: black; font-size: 11px;">${data[i].Numero_de_torniquete}</td>
+              <td class="table-success" style="background-color: white; border: solid 2px black; color: black; font-size: 11px; font-family: monospace;">${data[i].Nombre_completo}</td>
+              <td class="table-success" style="background-color: white; border: solid 2px black; color: black; font-size: 11px; font-family: monospace;">${data[i].Rut}</td>
+              <td class="table-success" style="background-color: white; border: solid 2px black; color: black; font-size: 11px; font-family: monospace;">${data[i].Carrera}</td>
+              <td class="table-success" style="background-color: white; border: solid 2px black; color: black; font-size: 11px; font-family: monospace;">${data[i].Hora_entrada}</td>
+              <td class="table-success" style="background-color: white; border: solid 2px black; color: black; font-size: 11px; font-family: monospace;">${data[i].Hora_salida}</td>
+              <td class="table-success" style="background-color: white; border: solid 2px black; color: black; font-size: 11px; font-family: monospace;">${data[i].Numero_de_torniquete}</td>
           </tr>`;
       }
       document.getElementById('AlumnosBody').innerHTML = body;
   };
 }
+
